7481ecbc5cd51fa131bb496ae828c4e373f7e303
Shun712 <sh705vi@gmail.com>
2022-09-09 21:48:54 +0900

    注文を登録

--- app/controllers/api/v1/line_foods_controller.rb
+++ app/controllers/api/v1/line_foods_controller.rb
@@ -1,14 +1,15 @@
 module Api
   module V1
     class LineFoodsController < ApplicationController
+      before_action :set_food, only: %i[create replace]
       def index
         line_foods = LineFood.active
         if line_foods.exists?
           render json: {
-            line_foods_ids: line_foods.map(&:id),
+            line_food_ids: line_foods.map { |line_food| line_food.id },
             restaurant: line_foods[0].restaurant,
-            count: line_foods.sum(&:count),
-            amount: line_foods.sum(&:total_amount)
+            count: line_foods.sum { |line_food| line_food[:count] },
+            amount: line_foods.sum { |line_food| line_food.total_amount },
           }, status: :ok
         else
           render json: {}, status: :no_content
@@ -16,16 +17,13 @@ module Api
       end
 
       def create
-        @ordered_food = Food.find(params[:food_id])
-        # すでに他店舗の仮予約があれば、処理を中止
         if LineFood.active.other_restaurant(@ordered_food.restaurant.id).exists?
           return render json: {
             existing_restaurant: LineFood.other_restaurant(@ordered_food.restaurant.id).first.restaurant.name,
-            new_restaurant: Food.find([:food_id]).restaurant.name
+            new_restaurant: Food.find(params[:food_id]).restaurant.name,
           }, status: :not_acceptable
         end
 
-        # 予約するfoodをセットする
         set_line_food(@ordered_food)
 
         if @line_food.save
@@ -38,9 +36,8 @@ module Api
       end
 
       def replace
-        @ordered_food = Food.find(params[:food_id])
-        Linefood.active.other_restaurant(@ordered_food.restaurant.id).each do |line_food|
-          line_food.update(:active, false)
+        LineFood.active.other_restaurant(@ordered_food.restaurant.id).each do |line_food|
+          line_food.update_attribute(:active, false)
         end
 
         set_line_food(@ordered_food)
@@ -56,8 +53,11 @@ module Api
 
       private
 
+      def set_food
+        @ordered_food = Food.find(params[:food_id])
+      end
+
       def set_line_food(ordered_food)
-        # 同じfoodなら追加する数を更新
         if ordered_food.line_food.present?
           @line_food = ordered_food.line_food
           @line_food.attributes = {
@@ -65,7 +65,6 @@ module Api
             active: true
           }
         else
-          # 新規のfoodなら数を登録
           @line_food = ordered_food.build_line_food(
             count: params[:count],
             restaurant: ordered_food.restaurant,
